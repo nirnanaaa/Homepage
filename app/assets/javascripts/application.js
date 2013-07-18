a21a5e8079e06579b72c7a85c99925d192671e44
nirnanaaa <mosny@zyg.li>
2013-07-18 18:35:02 +0200

    ember initializer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,24 +1,15 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require handlebars
 //= require ember
 //= require ember-data
-//= require homepage
+//= require ./fkasper/homepage
 
-// for more details see: http://emberjs.com/guides/application/
-Homepage = Ember.Application.create();
+Homepage = Ember.Application.create({
+	rootElement: 'body',
+	LOG_TRANSITIONS: true,
+	LOG_TRANSITIONS_INTERNAL: true
+});
 
 //= require_tree .
