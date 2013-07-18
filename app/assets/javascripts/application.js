a3212cc47414cc5bb3faa83b5d7887d9c5bf24c4
nirnanaaa <mosny@zyg.li>
2013-07-18 18:27:04 +0200

    initialized ember totally mess

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require homepage
+
+// for more details see: http://emberjs.com/guides/application/
+Homepage = Ember.Application.create();
+
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,14 @@ module Homepage
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
+    config.time_zone = 'Berlin'
+    
+    config.handlebars.templates_root = 'fkasper/templates'
+    config.exceptions_app = self.routes
+    config.active_record.schema_format = :sql
+    
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    #config.i18n.default_locale = :de
   end
 end
